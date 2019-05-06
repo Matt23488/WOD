76ef16bca0b2062855df51d6e5f79a39878876a3
Matthew Bryant <matt23488@gmail.com>
2019-05-06 11:13:05 -0400

    Dice rolling is animated

--- js/Dice.js
+++ js/Dice.js
@@ -3,7 +3,10 @@ $(function () {
         var self = this;
         self.dicePool = ko.observable(1);
         self.rollRounds = ko.observableArray([]);
+        self.rollingInProgress = ko.observable(false);
         self.totalDiceSuccesses = ko.computed(function () {
+            if (self.rollingInProgress() || self.rollRounds().length === 0) return "???";
+
             return self.rollRounds().reduce(function (total, r) {
                 return total + r.reduce(function (total, d) {
                     return total + (d > 7 ? 1 : 0);
@@ -19,40 +22,93 @@ $(function () {
     };
     
     Dice.prototype.rollDice = function () {
-        var self = this;
-        self.rollRounds.removeAll();
-        var numRolls = self.dicePool();
-        while (numRolls > 0) {
-            var rerolls = 0;
-            var currentRound = [];
-            for (var i = 0; i < numRolls; i++) {
-                var roll = randomInteger(1, 11);
-                if (roll === 10) rerolls++;
-                currentRound.push(roll)
-            }
-            self.rollRounds.push(currentRound);
-            numRolls = rerolls;
-        }
+        var roller = new RollState(this);
+        roller.beginRoll();
     };
 
     ko.bindingHandlers.dice = {
         update: function (element, valueAccessor) {
-            element.innerHTML = "";
             var rollRounds = valueAccessor()();
+            if (rollRounds.length === 0) {
+                element.innerHTML = "";
+                return;
+            }
+
+            var divs = element.getElementsByClassName("dice-round");
+            if (divs.length !== rollRounds.length) {
+                var newRound = document.createElement("div");
+                newRound.classList.add("dice-round");
+                var rolls = rollRounds[rollRounds.length - 1];
+                for (var i = 0; i < rolls.length; i++) {
+                    var span = document.createElement("span");
+                    var delay = randomFloat(0, 0.5);
+                    span.style.animation = "rollingDice 0.5s linear -" + delay + "s infinite alternate";
+                    span.classList.add("dice");
+                    newRound.appendChild(span);
+                }
+                element.appendChild(newRound);
+                divs = element.getElementsByClassName("dice-round");
+            }
+
             for (var i = 0; i < rollRounds.length; i++) {
                 var rolls = rollRounds[i];
-                var div = document.createElement("div");
-                div.classList.add("dice-round");
+                var spans = divs[i].getElementsByClassName("dice");
                 for (var j = 0; j < rolls.length; j++) {
                     var roll = rolls[j];
-                    var img = document.createElement("img");
-                    img.src = "images/dice-" + roll + ".png";
-                    img.classList.add("dice");
-                    div.appendChild(img);
+                    var span = spans[j];
+
+                    if (roll !== -1) {
+                        span.style.animation = null;
+                        span.style.backgroundImage = "url('images/dice-" + roll + ".png')";
+                    }
                 }
-                element.appendChild(div);
             }
         }
+    };
+
+    function RollState(diceObj) {
+        var self = this;
+        self.diceObj = diceObj;
+        self.currentRound = 0;
+        self.currentDice = 0;
+        self.currentRoundRolls = diceObj.dicePool();
+        self.currentRoundResults = [];
+        self.nextRoundRolls = 0;
+        self.intervalHandle = null;
+    }
+
+    RollState.prototype.beginRoll = function () {
+        var self = this;
+        self.diceObj.rollingInProgress(true);
+        self.diceObj.rollRounds.removeAll();
+        for (var i = 0; i < self.currentRoundRolls; i++) self.currentRoundResults.push(-1);
+        self.diceObj.rollRounds.push(self.currentRoundResults);
+
+        self.intervalHandle = window.setInterval(function () {
+
+            var nextRoll = randomInteger(1, 11);
+            if (nextRoll === 10) self.nextRoundRolls++;
+            self.currentRoundResults[self.currentDice] = nextRoll;
+            self.diceObj.rollRounds.replace(self.diceObj.rollRounds()[self.currentRound], self.currentRoundResults);
+            self.currentDice++;
+
+            if (self.currentDice === self.currentRoundRolls) {
+                if (self.nextRoundRolls === 0) {
+                    window.clearInterval(self.intervalHandle);
+                    self.diceObj.rollingInProgress(false);
+                    return;
+                }
+                else {
+                    self.currentRound++;
+                    self.currentRoundRolls = self.nextRoundRolls;
+                    self.nextRoundRolls = 0;
+                    self.currentDice = 0;
+                    self.currentRoundResults = [];
+                    for (var i = 0; i < self.currentRoundRolls; i++) self.currentRoundResults.push(-1);
+                    self.diceObj.rollRounds.push(self.currentRoundResults);
+                }
+            }
+        }, 250);
     };
 
     window.Dice = Dice;
--- js/utils.js
+++ js/utils.js
@@ -11,5 +11,18 @@ $(function () {
         return Math.floor(Math.random() * (maxExclusive - minInclusive)) + minInclusive;
     }
 
+    function randomFloat(minInclusive, maxExclusive) {
+        if (!minInclusive) minInclusive = 0;
+        if (!maxExclusive) maxExclusive = 1;
+        if (maxExclusive < minInclusive) {
+            var temp = minInclusive;
+            minInclusive = maxExclusive;
+            maxExclusive = temp;
+        }
+
+        return Math.random() * (maxExclusive - minInclusive) + minInclusive;
+    }
+
     window.randomInteger = randomInteger;
+    window.randomFloat = randomFloat;
 });
